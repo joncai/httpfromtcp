a7282e914025b0f3fa917d99ffdc444894b25d64
Jonathan (Jun) Cai <5146810+joncai@users.noreply.github.com>
2026-03-07 09:49:32 -0800

    Read 8-byte chunks

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil && err.Error() != "EOF" {
+			fmt.Println(err)
+			return
+		}
+		if n == 0 {
+			break
+		}
+		fmt.Printf("read: %s\n", string(buffer[:n]))
+	}
 }
